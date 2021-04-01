888d48f92f69d32d0484cf365f9a895510da2c55
Code_Zealot <business@codezealot.com>
2021-04-01 12:51:45 -0400

    added command cooldowns

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { prefix, token } = require("./config.json");
 const fs = require("fs");
 
 const client = new Discord.Client();
+client.cooldowns = new Discord.Collection();
 
 //Start Command Handler
 client.commands = new Discord.Collection();
@@ -50,6 +51,34 @@ client.on("message", (message) => {
 
     return message.channel.send(reply);
   }
+
+  //Start cooldowns
+  const { cooldowns } = client;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(
+          1
+        )} more second(s) before reusing the \`${command.name}\` command.`
+      );
+    }
+  }
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  //End cooldowns
 
   try {
     command.execute(message, args);
